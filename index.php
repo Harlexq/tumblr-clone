f2d7e21964484dd0bdfd59db928c27983b024cda
Harlexq <serhanbakir5@gmail.com>
2023-07-10 12:31:57 +0300

    tumblr-clone

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serhan Bakır</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+
+    <header class="header">
+        <div class="container-fluid header-inner">
+            <div class="navbar-brand">
+                <img src="assets/img/logo.png" alt="">
+                <div class="search">
+                    <button class="search-btn" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    <input class="search-input" type="text" placeholder="Tumblr'ı Ara">
+                </div>
+            </div>
+            <nav class="nav">
+                <menu class="navbar">
+                    <a class="nav-item active" href="#"><i class="fa-solid fa-house"></i></a>
+                    <a class="nav-item" href="#"><i class="fa-regular fa-compass"></i></a>
+                    <a class="nav-item" href="#"><i class="fa-solid fa-store"></i></a>
+                    <a class="nav-item" href="#"><i class="fa-solid fa-envelope"></i></a>
+                    <a class="nav-item" href="#"><i class="fa-solid fa-face-smile"></i></a>
+                    <a class="nav-item" href="#"><i class="fa-solid fa-bolt-lightning"></i></a>
+                    <a class="nav-item" href="#"><i class="fa-solid fa-user"></i></a>
+                    <a class="nav-item nav-pencil" href="#"><i class="fa-solid fa-pencil"></i></a>
+                </menu>
+            </nav>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="write-text">
+            <div class="write-text-inner">
+                <div class="write-text-content">
+                    <div class="write-text-user">
+                        <img src="assets/img/SerhanBakir.jpg" alt="">
+                    </div>
+                    <div class="write-text-info">
+                        <div class="write-info-inner">
+                            <menu class="write-info-menu">
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/metin.png" alt="">
+                                    <p>Metin</p>
+                                </a>
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/fotograf.png" alt="">
+                                    <p>Fotoğraf</p>
+                                </a>
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/alinti.png" alt="">
+                                    <p>Alıntı</p>
+                                </a>
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/baglanti.png" alt="">
+                                    <p>Bağlantı</p>
+                                </a>
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/sohbet.png" alt="">
+                                    <p>Sohbet</p>
+                                </a>
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/ses.png" alt="">
+                                    <p>Ses</p>
+                                </a>
+                                <a href="#" class="write-info-item">
+                                    <img src="assets/img/video.png" alt="">
+                                    <p>Video</p>
+                                </a>
+                            </menu>
+                        </div>
+                        <div class="user-btn">
+                            <div class="user-btn-inner">
+                                <menu class="user-btn-menu">
+                                    <a class="user-btn-item active" href="#">Senin İçin</a>
+                                    <a class="user-btn-item" href="#">Takip edilenler</a>
+                                    <a class="user-btn-item" href="#">Etiketlerin</a>
+                                    <a class="user-btn-item" href="#">Yönet...</a>
+                                </menu>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <aside class="browse">
+                    <div class="browse-inner">
+                        <h1>Bu bloglara göz at</h1>
+                        <div class="follow">
+                            <!-- <a href="#" class="follow-item">
+                                <img src="assets/img/follow1.jpg" alt="">
+                                <p>fotokopicibierkek</p>
+                                <p class="follow-text-color">Takip Et</p>
+                                <i class="fa-solid fa-x"></i>
+                            </a>
+                            <a href="#" class="follow-item">
+                                <img src="assets/img/follow1.jpg" alt="">
+                                <p>fotokopicibierkek</p>
+                                <p class="follow-text-color">Takip Et</p>
+                                <i class="fa-solid fa-x"></i>
+                            </a>
+                            <a href="#" class="follow-item">
+                                <img src="assets/img/follow1.jpg" alt="">
+                                <p>fotokopicibierkek</p>
+                                <p class="follow-text-color">Takip Et</p>
+                                <i class="fa-solid fa-x"></i>
+                            </a>
+                            <a href="#" class="follow-item">
+                                <img src="assets/img/follow1.jpg" alt="">
+                                <p>fotokopicibierkek</p>
+                                <p class="follow-text-color">Takip Et</p>
+                                <i class="fa-solid fa-x"></i>
+                            </a> -->
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
