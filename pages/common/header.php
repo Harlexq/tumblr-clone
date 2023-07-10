703b4109cc6870fabd3da88c426aaf004be801b3
Harlexq <serhanbakir5@gmail.com>
2023-07-10 22:51:55 +0300

    tumblr-clone

---
+++ pages/common/header.php
@@ -0,0 +1,23 @@
+<header class="header">
+    <div class="container-fluid header-inner">
+        <div class="navbar-brand">
+            <img src="assets/img/logo.png" alt="">
+            <form class="search">
+                <input class="search-input" type="text" placeholder="Tumblr'ı Ara">
+                <button class="search-btn" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+        <nav class="nav">
+            <menu class="navbar">
+                <a class="nav-item active" href="#"><i class="fa-solid fa-house"></i></a>
+                <a class="nav-item" href="#"><i class="fa-regular fa-compass"></i></a>
+                <a class="nav-item" href="#"><i class="fa-solid fa-store"></i></a>
+                <a class="nav-item" href="#"><i class="fa-solid fa-envelope"></i></a>
+                <a class="nav-item" href="#"><i class="fa-solid fa-face-smile"></i></a>
+                <a class="nav-item" href="#"><i class="fa-solid fa-bolt-lightning"></i></a>
+                <a class="nav-item" href="#"><i class="fa-solid fa-user"></i></a>
+                <a class="nav-item nav-pencil" href="#"><i class="fa-solid fa-pencil"></i></a>
+            </menu>
+        </nav>
+    </div>
+</header>
