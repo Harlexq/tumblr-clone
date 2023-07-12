63856ed2a0e91043fdd47c38a0cbf13c094df6bc
Harlexq <serhanbakir5@gmail.com>
2023-07-12 16:54:13 +0300

    tumblr-clone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,48 @@
-function toggleSmileMenu() {
-    const smileMenu = document.getElementById("smile");
-    const lightningMenu = document.getElementById("lightning");
-
-    smileMenu.style.display = smileMenu.style.display === "block" ? "none" : "block";
-    lightningMenu.style.display = "none";
-}
-
 function toggleLightningMenu() {
-    const smileMenu = document.getElementById("smile");
     const lightningMenu = document.getElementById("lightning");
+    const navNightningMenu = document.getElementById("navLightning");
 
-    lightningMenu.style.display = lightningMenu.style.display === "block" ? "none" : "block";
-    smileMenu.style.display = "none";
+    if (!navNightningMenu.classList.contains("show")) {
+        navNightningMenu.classList.add("show");
+        lightningMenu.style.display = "block";
+    } else {
+        navNightningMenu.classList.remove("show");
+        lightningMenu.style.display = "none";
+    }
 }
+
+document.addEventListener("click", function (event) {
+    const lightningMenu = document.getElementById("lightning");
+    const navLightningDiv = document.getElementById("navLightning");
+
+    if (!event.target.closest("#lightning") && !event.target.closest("#navLightning")) {
+        navLightningDiv.classList.remove("show");
+        lightningMenu.style.display = "none";
+    }
+});
+
+function toggleSmileMenu() {
+    const smileDiv = document.getElementById("smile");
+    const navSmileDiv = document.getElementById("navSmile");
+
+    if (!navSmileDiv.classList.contains("show")) {
+        navSmileDiv.classList.add("show");
+        smileDiv.style.display = "block";
+    } else {
+        navSmileDiv.classList.remove("show");
+        smileDiv.style.display = "none";
+    }
+}
+
+document.addEventListener("click", function (event) {
+    const smileMenu = document.getElementById("smile");
+    const navSmileDiv = document.getElementById("navSmile");
+
+    if (!event.target.closest("#smile") && !event.target.closest("#navSmile")) {
+        navSmileDiv.classList.remove("show");
+        smileMenu.style.display = "none";
+    }
+});
 
 function toggleUserMenu() {
     const userDiv = document.getElementById("user");
